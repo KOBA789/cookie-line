222b8ec020ae0db0041f11b4d5c3da36ad6600fa
koba789 <kobahide789@gmail.com>
2012-01-08 15:16:24 +0900

    add removing cookie to usecase.js

--- test/usecase.js
+++ test/usecase.js
@@ -4,5 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   var cookies = cookie(req, res);
   cookies.set('hoge', 'bar', {domain: '.local.koba789.com'});
+//  cookies.remove('hoge');
   res.end(cookies.get('hoge'));
 }).listen(8124);
