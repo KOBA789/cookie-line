88288b9b577ba8d1c557f5638b62b2a73aadcaef
koba789 <kobahide789@gmail.com>
2012-07-02 18:00:49 +0900

    support writing multiple cookies

--- test/usecase.js
+++ test/usecase.js
@@ -4,6 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   var cookies = cookie(req, res);
   cookies.set('hoge', 'bar', {domain: '.local.koba789.com'});
+  cookies.set('huga', 'baz', {domain: '.local.koba789.com', path: '/'});
 //  cookies.remove('hoge');
-  res.end(cookies.get('hoge'));
-}).listen(8124);
+  res.end(cookies.get('hoge') + ', ' + cookies.get('huga'));
+}).listen(8126);
